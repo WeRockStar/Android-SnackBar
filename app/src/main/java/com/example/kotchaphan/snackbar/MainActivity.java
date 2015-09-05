3d679c9a4abe7b1b0bd419b9bd14f844dc13a36b
WeRockStar <vr_freestyle@outlook.com>
2015-09-06 01:35:20 +0700

    custom snack bar

--- app/src/main/java/com/example/kotchaphan/snackbar/MainActivity.java
+++ app/src/main/java/com/example/kotchaphan/snackbar/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.kotchaphan.snackbar;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Html;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.StyleSpan;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,7 +30,23 @@ public class MainActivity extends AppCompatActivity {
 //                .setAction("undo", clickListener)
 //                .show();
 
-        Snackbar.make(coordinatorLayut, Html.fromHtml("Add <b>bold</b> to Snackbar text"), Snackbar.LENGTH_LONG).show();
+        //  Snackbar.make(coordinatorLayut, Html.fromHtml("Add <b>bold</b> to Snackbar text"), Snackbar.LENGTH_LONG).show();
+
+        SpannableStringBuilder snackBarText = new SpannableStringBuilder();
+        snackBarText.append("Add ");
+        int bold = snackBarText.length();
+        snackBarText.append("bold color");
+        snackBarText.setSpan(new ForegroundColorSpan(0xFFFF0000)
+                , bold, snackBarText.length()
+                , Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        snackBarText.setSpan(new StyleSpan(Typeface.BOLD)
+                , bold, snackBarText.length()
+                , Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        snackBarText.append(" to SnackBar Text");
+
+        Snackbar.make(coordinatorLayut , snackBarText , Snackbar.LENGTH_LONG)
+                .setAction("Reply" , clickListener)
+                .show();
 
     }
 
